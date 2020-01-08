f5b982dcc0d90c3527867264a55c5d6caf3d6b6e
Guilherme André Welter <guilherme.a.welter@gmail.com>
2020-01-08 07:40:29 -0300

    Using DTOs and deleting a Task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid/v1';
+import { CreateTaskDTO } from './dto/create-task-.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,13 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string) {
+  getTaskById(id: string): Task {
+    const [task, _] = this.findTaskById(id);
+    return { ...task };
+  }
+
+  createTask(createTaskDTO: CreateTaskDTO) {
+    const { title, description } = createTaskDTO;
     const task: Task = {
       id: uuid(),
       title,
@@ -20,5 +27,24 @@ export class TasksService {
 
     this.tasks.push(task);
     return task;
+  }
+
+  deleteTask(id: string) {
+    const [task, taskIndex] = this.findTaskById(id);
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+    this.tasks.splice(taskIndex, 1);
+    return task;
+  }
+
+  private findTaskById(id: string): [Task, number] {
+    const taskIndex = this.tasks.findIndex(t => t.id === id);
+    const task = this.tasks[taskIndex];
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return [task, taskIndex];
   }
 }
